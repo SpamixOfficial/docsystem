0e79915aaa7ba2513d609e4aba83a081865a3ffd
SpamixOfficial <99183771+SpamixOfficial@users.noreply.github.com>
2024-06-05 21:12:14 +0200

    add pm2 config

---
+++ pm2.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "docsystem", // Name of your application
+    script: "build/index.js", // Entry point of your application
+    interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
+  };
+  
